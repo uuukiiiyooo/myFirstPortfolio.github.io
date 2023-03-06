6dba216f48080e00829362f915e6cc1bc00f2ed9
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-03-07 03:56:15 +0500

    add javascript

--- home.js
+++ home.js
@@ -1,5 +1,21 @@
 let menuIcon = document.querySelector('.menuIcon');
+let menuText = document.querySelector('.menuText');
+let portfolioAnchor = document.querySelector('#portfolioMenuButton');
+let aboutAnchor = document.querySelector('#aboutMenuButton');
+let contactAnchor = document.querySelector('#contactMenuButton');
 
 menuIcon.addEventListener("click", function() {
-    console.log("This works!");
+    menuText.classList.add('menuTextToggle');
+})
+
+portfolioAnchor.addEventListener("click", function() {
+    menuText.classList.remove('menuTextToggle');
+})
+
+aboutAnchor.addEventListener("click", function() {
+    menuText.classList.remove('menuTextToggle');
+})
+
+contactAnchor.addEventListener("click", function() {
+    menuText.classList.remove('menuTextToggle');
 })
