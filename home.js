e043cfd62c0d270935d21dd29462c016a468a44b
uuukiiiyooo <uuukiiiyooo@gmail.com>
2023-03-06 19:05:37 -0600

    Update style to look as in the copied template

--- home.js
+++ home.js
@@ -1,11 +1,16 @@
 let menuIcon = document.querySelector('.menuIcon');
 let menuText = document.querySelector('.menuText');
+let closeMenuButton = document.querySelector('#closeMenuButton');
 let portfolioAnchor = document.querySelector('#portfolioMenuButton');
 let aboutAnchor = document.querySelector('#aboutMenuButton');
 let contactAnchor = document.querySelector('#contactMenuButton');
 
 menuIcon.addEventListener("click", function() {
     menuText.classList.add('menuTextToggle');
+})
+
+closeMenuButton.addEventListener("click", function() {
+    menuText.classList.remove('menuTextToggle');
 })
 
 portfolioAnchor.addEventListener("click", function() {
